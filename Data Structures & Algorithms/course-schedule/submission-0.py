1ab84d7493f86f5bf75e7b7573550d8d08a4f2ef
Sathvik Mallela <94075194+Sathvik6323@users.noreply.github.com>
2026-07-24 03:38:38 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.py
@@ -0,0 +1,33 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        premap = {i :[] for i in range(numCourses)}
+
+        for crs , pre in prerequisites:
+            premap[crs].append(pre)
+
+        #visitset = all courses along curr dfs path save if repeated then loop 
+        visit= set()
+
+        def dfs(crs):
+            if crs in visit:
+                return False
+            if premap[crs]==[]:
+                return True
+
+            visit.add(crs)
+            for pre in premap[crs]:
+                if not dfs(pre):
+                    return False
+            
+            visit.remove(crs)
+            premap[crs]=[]
+            return True
+        
+        for crs in range(numCourses):
+            if not dfs(crs): return False
+        
+        return True
+
+        
+
+        
